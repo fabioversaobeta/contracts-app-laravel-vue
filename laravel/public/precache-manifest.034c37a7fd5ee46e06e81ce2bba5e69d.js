537a28b2bbcea290ee50c18ab9bbd5725b5286f5
Fabio Bandacheski <fabio.versao.beta@gmail.com>
2020-08-21 13:05:42 -0300

    vue start, config and build

---
+++ laravel/public/precache-manifest.034c37a7fd5ee46e06e81ce2bba5e69d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "162604ec41be1eeb9b70",
+    "url": "/css/app.5f70efee.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f56d510709b04c17f2f834b95f7e48f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd35b2bc23a4a908e2fd",
+    "url": "/js/about.6d981060.js"
+  },
+  {
+    "revision": "162604ec41be1eeb9b70",
+    "url": "/js/app.80cfae43.js"
+  },
+  {
+    "revision": "6468c4f7ced6b564b4a8",
+    "url": "/js/chunk-vendors.0a24a9c0.js"
+  },
+  {
+    "revision": "4b14c64efaf846819b9a229b4193c8b7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
