97992e113454689b4d68a0361d4661b5e92aa1f9
Fabio Bandacheski <fabio.versao.beta@gmail.com>
2020-08-30 21:25:26 -0300

    cors

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -8,19 +8,19 @@ use Illuminate\Support\Facades\Route;
 
 // Route::get('/{any}', 'SpaController@index')->where('any', '.*');
 
-$router->group(['prefix' => 'property'], function() use ($router) {
+$router->group(['middleware' => 'cors', 'prefix' => 'property'], function() use ($router) {
     $router->get('/', 'PropertyController@show');
     $router->post('/', 'PropertyController@create');
     $router->delete('/{id}', 'PropertyController@delete');
     $router->put('/{id}/setContract/', 'PropertyController@setContract');
 });
 
-$router->group(['prefix' => 'contractor'], function() use ($router) {
+$router->group(['middleware' => 'cors', 'prefix' => 'contractor'], function() use ($router) {
     $router->get('/', 'ContractorController@show');
     $router->post('/', 'ContractorController@create');
 });
 
-$router->group(['prefix' => 'contract'], function() use ($router) {
+$router->group(['middleware' => 'cors', 'prefix' => 'contract'], function() use ($router) {
     $router->get('/', 'ContractController@show');
     $router->post('/', 'ContractController@create');
 });
